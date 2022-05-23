b70ac5f3a82714082cabf7ad7cafc736e48c2312
hondasan <86158971+hondasan@users.noreply.github.com>
2022-05-23 22:48:10 +0900

    Contextでのstate管理

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,12 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
   const { children } = props;
-  const contextName = "兄貴";
+  const [userInfo, setUserInfo] = useState(null);
+
   return (
-    <UserContext.Provider value={{ contextName }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   );
